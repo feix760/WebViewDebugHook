918e926a1162c1531f434fc7446ab93d52d86ea4
飞羏 <feixiang.yfx@alibaba-inc.com>
2019-03-25 14:12:20 +0800

    version 2.0: support for x5/u4 webview

--- README.md
+++ README.md
@@ -15,17 +15,17 @@ But the most app are third parties, webview debugging is disabled. WebViewDebugH
 
 - Installed [Xposed](http://repo.xposed.info/module/de.robv.android.xposed.installer)
 
-## Note
+## Xposed module Download
 
-- libs/XposedBridgeApi-XX.jar should not build to app file.
+- [WebViewDebugHook.apk](app/release/app-release.apk)
 
-- Tencent QQ use it's X5 kernel, and could not debugging. We can make a file named `debug.conf` with blow content in the root directory to force it use webview.
+## Supported Webview
 
-```ini
-    result_QProxy=false
-    result_systemWebviewForceUsed=true
-    setting_forceUseSystemWebview=true
-```
+- Android Webview
+- Tencent X5 Webview
+- UC U4/U3 Webview
+
+*You can post an issue to support more webview*
 
 ## Question
 
@@ -33,6 +33,11 @@ But the most app are third parties, webview debugging is disabled. WebViewDebugH
 
 Xposed is a framework for modules that can change the behavior of the system and apps without touching any APKs. That's great because it means that modules can work for different versions and even ROMs without any changes (as long as the original code was not changed too much). It's also easy to undo. As all changes are done in the memory, you just need to deactivate the module and reboot to get your original system back. There are many other advantages, but here is just one more: Multiple modules can do changes to the same part of the system or app. With modified APKs, you to decide for one. No way to combine them, unless the author builds multiple APKs with different combinations.
 <br><br>Please go http://repo.xposed.info/module/de.robv.android.xposed.installer for details.
+
+## Links
+
+- [Xposed Development tutorial](https://github.com/rovo89/XposedBridge/wiki/Development-tutorial)
+- [Tencent X5 SDK](https://x5.tencent.com/tbs/guide/sdkInit.html)
 
 ## License
 
---
+++ app/src/main/java/club/fishine/webviewdebughook/WebViewHook.java
@@ -0,0 +1,49 @@
+package club.fishine.webviewdebughook;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+public class WebViewHook implements IXposedHookLoadPackage {
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("Loaded app: " + lpparam.packageName);
+        final String packageName = lpparam.packageName;
+
+        // android webview & tencent x5 & UC
+        final String[] webviewList = {"android.webkit.WebView", "com.tencent.smtt.sdk.WebView", "com.uc.webview.export.WebView"};
+        for (int i = 0; i < webviewList.length; i++) {
+
+            final String className = webviewList[i];
+
+            Class findCla = null;
+
+            try {
+                findCla = XposedHelpers.findClass(className, lpparam.classLoader); // will throw exception
+            } catch (Throwable exception) {
+            }
+
+            if (findCla != null) {
+                final Class cla = findCla;
+
+                XposedBridge.hookAllConstructors(cla, new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedHelpers.callStaticMethod(cla, "setWebContentsDebuggingEnabled", true);
+                        XposedBridge.log(className + "(), " + packageName);
+                    }
+                });
+
+                XposedBridge.hookAllMethods(cla, "setWebContentsDebuggingEnabled", new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        param.args[0] = true;
+                        XposedBridge.log(className + ".setWebContentsDebuggingEnabled(), " + packageName);
+                    }
+                });
+            }
+        }
+
+    }
+}
